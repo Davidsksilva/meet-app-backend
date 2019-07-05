6dfb802a0a09224e23d796ae32d7da02bf4402e0
David <davidsimonkastle@gmail.com>
2019-07-04 22:57:38 -0300

    feature: jwt authentication, update endpoint

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,7 +15,7 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation falied.' });
+      return res.status(400).json({ error: 'Validation failed.' });
     }
 
     const userExists = await User.findOne({ where: { email: req.body.email } });
@@ -25,6 +25,52 @@ class UserController {
     }
 
     const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const emailTaken = await User.findOne({ where: email });
+
+      if (emailTaken) {
+        return res.status(400).json({ error: 'E-mail already used.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = await user.update(req.body);
 
     return res.json({
       id,
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: ' f8dc25ad4ba932614e125c55f07e3ccb',
+  expiresIn: '7d',
+};
